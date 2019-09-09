f2728a61af7ba189243a34b9192d788c4cd7f8db
Swanndolia <Swanndolia@gmail.com>
2019-09-09 02:03:21 +0200

    fields adds
    add two fields and detect his content better background

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,6 +1,12 @@
 package application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Window;
 
 public class Controller 
 {
@@ -9,4 +15,49 @@ public class Controller
 	{
 		System.exit(0);
 	}
-}
+	
+	@FXML
+	private void appMinimize() 
+	{
+        Main.stage.setIconified(true);
+	}
+	
+	@FXML
+	private void openSettings() 
+	{
+		System.exit(0);
+	}
+	
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private PasswordField passField;
+
+    @FXML
+    private Button loginButton;
+
+	
+    @FXML
+	 protected void login(ActionEvent event) {
+	        Window owner = loginButton.getScene().getWindow();
+	        if(nameField.getText().isEmpty()) {
+	            showAlert(Alert.AlertType.ERROR, owner, "Invalid Fields", "Enter your mojang email or an username for normal auth");
+	            return;
+	        }
+	        
+	        if(passField.getText().isEmpty())
+	        	System.out.print("not premium");
+	        
+	        showAlert(Alert.AlertType.CONFIRMATION, owner, "Login Succes",  "Welcome " + nameField.getText());
+	    }
+    
+    public static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
+    }
+	}
--- src/application/Main.java
+++ src/application/Main.java
@@ -3,12 +3,14 @@ package application;
 import java.io.IOException;
 import java.net.URL;
 
+import javax.swing.JTextField;
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
@@ -21,44 +23,44 @@ public final class Main extends Application {
     private double xOffset = 0;
     private double yOffset = 0;
 	
-	  @Override
-	public void start(final Stage primaryStage) {
-		try {
-	    	URL url = getClass().getResource("/ui/main.fxml");
-	    	FXMLLoader fxmlLoader = new FXMLLoader(url);
-	    	AnchorPane root = (AnchorPane) fxmlLoader.load();
-	        final Scene scene = new Scene(root, 1300, 700);
-	        final Rectangle rect = new Rectangle(1300,700);
-	        primaryStage.setScene(scene);
-	        scene.getStylesheets().add("/ui/application.css");
-		    rect.setArcHeight(60.0);
-	        rect.setArcWidth(60.0);
-	        root.setClip(rect);
-	        scene.setFill(Color.TRANSPARENT);
-
-	        primaryStage.initStyle(StageStyle.TRANSPARENT);
-	        primaryStage.setTitle("azurpixel v4");
-	        primaryStage.setScene(scene);
-	        primaryStage.show();
-	        
-	        //mover
-	        root.setOnMousePressed(new EventHandler<MouseEvent>() {
-	        	@Override
-	        	public void handle(MouseEvent event) {
-	        		xOffset = event.getSceneX();
-	        		yOffset = event.getSceneY();
-	        	}
-	        });
-	        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
-	        	@Override
-	        	public void handle(MouseEvent event) {
-	        		primaryStage.setX(event.getScreenX() - xOffset);
-	        		primaryStage.setY(event.getScreenY() - yOffset);
-	        	}
-	        });
-	    } catch (IOException ex) {}
+    public static Stage stage;
+    
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+	   	URL url = getClass().getResource("/ui/main.fxml");
+	   	FXMLLoader fxmlLoader = new FXMLLoader(url);
+	   	AnchorPane root = (AnchorPane) fxmlLoader.load();
+	    final Scene scene = new Scene(root, 1300, 700);
+	    final Rectangle rect = new Rectangle(1300,700);
+	    stage = primaryStage;
+	    stage.setScene(scene);
+	    scene.getStylesheets().add("/ui/application.css");
+		rect.setArcHeight(60.0);
+	    rect.setArcWidth(60.0);
+	    root.setClip(rect);
+	    scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setTitle("azurpixel v4");
+        stage.setScene(scene);
+        stage.show();
+     
+        //mover
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+        	@Override
+        	public void handle(MouseEvent event) {
+        		xOffset = event.getSceneX();
+        		yOffset = event.getSceneY();
+        	}
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+        	@Override
+        	public void handle(MouseEvent event) {
+        		stage.setX(event.getScreenX() - xOffset);
+        		stage.setY(event.getScreenY() - yOffset);
+        	}
+        });
 	  }
-	  
+
 	  public static void main(String[] args) {
 	    launch(args);
 	  }
